4c30ad00c335129017a3ecfc226797c7484ec0d3
light <119718848+ssynical@users.noreply.github.com>
2024-08-25 23:10:48 +1200

    Update pch.h

--- runluau-net/pch.h
+++ runluau-net/pch.h
@@ -2,8 +2,20 @@
 
 #include <stdio.h>
 #include <Windows.h>
+#include <string>
+#include <iostream>
 #include <chrono>
 #include <thread>
+#include <future>
+
+#define ASIO_STANDALONE
+#include <websocketpp/config/asio_client.hpp>
+#include <websocketpp/client.hpp>
+
+#include <boost/beast.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/stream.hpp>
 
 extern "C" {
     #include "lua.h"
