0a06a5aa32e2580dd0b6782997199265bd45fc79
dumpstring <100595391+dumpstring@users.noreply.github.com>
2024-09-01 11:40:09 +0200

    overwrote net library, added project files. will be writing from scratch.

--- runluau-net/lib.cpp
+++ runluau-net/lib.cpp
@@ -1,85 +1,15 @@
 #include "pch.h"
-#include "websocket_client.hpp"
-#include "http_client.hpp"
 
-#define wanted_arg_count(n) \
-if (n > 0 && lua_gettop(L) < n) [[unlikely]] { \
-    lua_pushfstring(L, __FUNCTION__" expects at least " #n " arguments, received %d", lua_gettop(L)); \
-    lua_error(L); \
-    return 0; \
-}
-
-
-static int ws_connect(lua_State* L) {
-    wanted_arg_count(1);
-    const char* uri = luaL_checkstring(L, 1);
-    
-    WebSocketClient* client = new WebSocketClient(uri);
-    lua_pushlightuserdata(L, client);
-    
-    client->connect_async();
-    
-    return lua_yield(L, 1);
-}
-
-static int ws_send(lua_State* L) {
-    wanted_arg_count(2);
-    WebSocketClient* client = (WebSocketClient*)lua_touserdata(L, 1);
-    const char* message = luaL_checkstring(L, 2);
-    
-    client->send(message);
-    return 0;
-}
-
-static int ws_receive(lua_State* L) {
-    wanted_arg_count(1);
-    WebSocketClient* client = (WebSocketClient*)lua_touserdata(L, 1);
-    
-    client->receive_async();
-    
-    return lua_yield(L, 1);
-}
-
-static int ws_close(lua_State* L) {
-    wanted_arg_count(1);
-    WebSocketClient* client = (WebSocketClient*)lua_touserdata(L, 1);
-    
-    client->close();
-    delete client;
-    return 0;
-}
-
-
-static int http_get(lua_State* L) {
-    wanted_arg_count(1);
-    const char* url = luaL_checkstring(L, 1);
-    
-    HttpClient::get_async(url);
-    
-    return lua_yield(L, 1);
-}
-
-static int http_post(lua_State* L) {
-    wanted_arg_count(2);
-    const char* url = luaL_checkstring(L, 1);
-    const char* data = luaL_checkstring(L, 2);
-    
-    HttpClient::post_async(url, data);
-    
-    return lua_yield(L, 1);
+int hello(lua_State* thread) {
+	printf("Working\n");
+	return 0;
 }
 
 #define reg(name) {#name, name}
 constexpr luaL_Reg library[] = {
-    reg(ws_connect),
-    reg(ws_send),
-    reg(ws_receive),
-    reg(ws_close),
-    reg(http_get),
-    reg(http_post),
-    {NULL, NULL}
+	reg(hello),
+	{NULL, NULL}
 };
-
-extern "C" __declspec(dllexport) void register_library(lua_State* L) {
-    luaL_register(L, "net", library);
+extern "C" __declspec(dllexport) void register_library(lua_State* thread) {
+	luaL_register(thread, "net", library);
 }
--- runluau-net/pch.h
+++ runluau-net/pch.h
@@ -2,22 +2,5 @@
 
 #include <stdio.h>
 #include <Windows.h>
-#include <string>
-#include <iostream>
-#include <chrono>
-#include <thread>
-#include <future>
 
-#define ASIO_STANDALONE
-#include <websocketpp/config/asio_client.hpp>
-#include <websocketpp/client.hpp>
-
-#include <boost/beast.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/ssl/stream.hpp>
-
-extern "C" {
-    #include "lua.h"
-    #include "lauxlib.h"
-}
+#include <luau.h>
