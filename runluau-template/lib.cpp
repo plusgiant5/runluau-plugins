90ea12aae9b5f15cc1d2418567193ec6d7335a7c
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-11-12 17:53:07 -0600

    chore(lib.cpp): ChatGPT converted the indentation to spaces :skull:

--- runluau-template/lib.cpp
+++ runluau-template/lib.cpp
@@ -1,14 +1,14 @@
 #include "pch.h"
 
 int test(lua_State* thread) {
-    printf("Working\n");
-    return 0;
+	printf("Working\n");
+	return 0;
 }
 
 #define reg(name) {#name, name}
 constexpr luaL_Reg library[] = {
-    reg(test),
-    {NULL, NULL}
+	reg(test),
+	{NULL, NULL}
 };
 
 extern "C"
@@ -18,5 +18,5 @@ __declspec(dllexport)
 __attribute__((visibility("default")))
 #endif
 void register_library(lua_State* thread) {
-    luaL_register(thread, "template", library);
+	luaL_register(thread, "template", library);
 }
