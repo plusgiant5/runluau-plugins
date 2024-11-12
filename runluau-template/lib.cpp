c6db8165af02d741d24b3a74755e687a23a5b8dd
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-11-12 17:49:35 -0600

    feat(runluau-template): ChatGPT porting Inc.

--- runluau-template/lib.cpp
+++ runluau-template/lib.cpp
@@ -1,15 +1,22 @@
 #include "pch.h"
 
 int test(lua_State* thread) {
-	printf("Working\n");
-	return 0;
+    printf("Working\n");
+    return 0;
 }
 
 #define reg(name) {#name, name}
 constexpr luaL_Reg library[] = {
-	reg(test),
-	{NULL, NULL}
+    reg(test),
+    {NULL, NULL}
 };
-extern "C" __declspec(dllexport) void register_library(lua_State* thread) {
-	luaL_register(thread, "template", library);
+
+extern "C"
+#ifdef _WIN32
+__declspec(dllexport)
+#else
+__attribute__((visibility("default")))
+#endif
+void register_library(lua_State* thread) {
+    luaL_register(thread, "template", library);
 }
